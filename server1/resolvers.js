de8f6289e10854437dbed1835374d82fdcc7d4f8
Jonathan <jtellkampf@gmail.com>
2021-08-25 12:46:03 +0100

    Created get user resolver

--- server1/resolvers.js
+++ server1/resolvers.js
@@ -1,6 +1,21 @@
+const User = require("./models/User");
+
 const resolvers = {
   Query: {
-    user: async (parent, args, context, info) => {},
+    user: async (parent, args, context, info) => {
+      const { id } = args;
+
+      const user = await User.findById(id);
+
+      return {
+        id: user._id,
+        name: user.name,
+        username: user.username,
+        email: user.email,
+        age: user.age,
+        gender: user.gender,
+      };
+    },
   },
 };
 
