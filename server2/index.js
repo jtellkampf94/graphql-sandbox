cdec20c7b5eef4f1ce1d39483f66c94a528e9594
Jonathan <jtellkampf@gmail.com>
2021-08-29 14:45:27 +0100

    Completed all the resolvers for the relational data of comments users and posts

--- server2/index.js
+++ server2/index.js
@@ -48,18 +48,26 @@ const comments = [
   {
     id: "102",
     text: "This worked well for me. Thanks!",
+    author: "3",
+    post: "10",
   },
   {
     id: "103",
     text: "Glad you enjoyed it.",
+    author: "1",
+    post: "10",
   },
   {
     id: "104",
     text: "This did not work.",
+    author: "2",
+    post: "11",
   },
   {
     id: "105",
     text: "Nevermind. I got it to work.",
+    author: "1",
+    post: "11",
   },
 ];
 
@@ -78,6 +86,7 @@ const typeDefs = gql`
     email: String!
     age: Int
     posts: [Post!]!
+    comments: [Comment!]!
   }
 
   type Post {
@@ -86,11 +95,14 @@ const typeDefs = gql`
     body: String!
     published: Boolean!
     author: User!
+    comments: [Comment!]!
   }
 
   type Comment {
     id: ID!
     text: String!
+    author: User!
+    post: Post!
   }
 `;
 
@@ -135,15 +147,32 @@ const resolvers = {
         published: false,
       };
     },
+    comments: (parent, args, context, info) => {
+      return comments;
+    },
   },
   Post: {
     author: (parent, args, context, info) => {
       return users.find((user) => user.id === parent.author);
     },
+    comments: (parent, args, context, info) => {
+      return comments.filter((comment) => comment.post === parent.id);
+    },
   },
   User: {
     posts: (parent, args, context, info) => {
       return posts.filter((post) => post.author === parent.id);
+    },
+    comments: (parent, args, context, info) => {
+      return comments.filter((comment) => comment.author === parent.id);
+    },
+  },
+  Comment: {
+    author: (parent, args, context, info) => {
+      return users.find((user) => user.id === parent.author);
+    },
+    post: (parent, args, context, info) => {
+      return posts.find((post) => post.id === parent.post);
     },
   },
 };
