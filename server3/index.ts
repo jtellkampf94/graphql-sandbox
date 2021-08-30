ea99117b3e0a22ff01547b60c8559f4233667ca0
Jonathan <jtellkampf@gmail.com>
2021-08-30 21:06:14 +0100

    Created server3 for prisma practice, created usermodel and first migration

---
+++ server3/index.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {};
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
