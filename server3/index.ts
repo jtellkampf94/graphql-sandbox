0ba3d879cec14859d63a95bda315206dd594bba0
Jonathan <jtellkampf@gmail.com>
2021-08-31 10:07:39 +0100

    Created Post model on prisma schema and created relationship between Post and User model

--- server3/index.ts
+++ server3/index.ts
@@ -2,7 +2,17 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const main = async () => {};
+const main = async () => {
+  const result = await prisma.user.update({
+    where: {
+      email: "pat@example.com",
+    },
+    data: {
+      name: "patrick",
+    },
+  });
+  console.log(result);
+};
 
 main()
   .catch((e) => {
