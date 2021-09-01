bc99e18cdd2b909b1319b0fc69a9fb9f89a3112f
Jonathan <jtellkampf@gmail.com>
2021-09-01 13:22:18 +0100

    Practice some more prisma client queries on server 3

--- server3/index.ts
+++ server3/index.ts
@@ -3,14 +3,36 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const main = async () => {
-  const result = await prisma.user.update({
-    where: {
-      email: "pat@example.com",
-    },
-    data: {
-      name: "patrick",
+  // Add relational data
+  // const result = await prisma.post.update({
+  //   where: {
+  //     id: 1,
+  //   },
+  //   data: {
+  //     author: {
+  //       connect: {
+  //         email: "pat@example.com",
+  //       },
+  //     },
+  //   },
+  // });
+
+  // Select desired fields
+  // const result = await prisma.user.findMany({
+  //   select: {
+  //     id: true,
+  //     name: true,
+  //   },
+  // });
+
+  // Include relational data
+  const result = await prisma.user.findUnique({
+    where: { email: "pat@example.com" },
+    include: {
+      posts: true,
     },
   });
+
   console.log(result);
 };
 
