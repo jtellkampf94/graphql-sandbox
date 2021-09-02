7f47e5598a491953d3c572e8a4b4b9949ca70168
Jonathan <jtellkampf@gmail.com>
2021-09-02 16:08:22 +0100

    Created register user resolver

--- server6/entity/User.ts
+++ server6/entity/User.ts
@@ -1,18 +1,28 @@
+import { Field, ID, ObjectType } from "type-graphql";
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 
+@ObjectType()
 @Entity()
 export class User extends BaseEntity {
+  @Field(() => ID)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   firstName: string;
 
+  @Field()
   @Column()
   lastName: string;
 
+  @Field()
   @Column("text", { unique: true })
   email: string;
+
+  @Field()
+  name: string;
+  //graphql type only
 
   @Column()
   password: string;
